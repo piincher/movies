ed0921e3403322b6d0f8c5b758fa2ea16d33a5bc
Ibrahim Kouma <ikouma269@gmail.com>
2021-08-04 00:22:47 +0800

    end of the movies project

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const autoCompleteConfig = {
 		return movie.Title;
 	},
 	async fetchData(searchTerm) {
-		f;
 		const response = await axios.get('http://www.omdbapi.com/', {
 			params: {
 				apikey: '66847f69',
@@ -67,10 +66,44 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 };
 
 const runComparison = () => {
-	console.log('Time for comparison');
-};
+	const leftSideStats = document.querySelectorAll(
+		'#left-summary .notification'
+	);
+	const rightSideStats = document.querySelectorAll(
+		'#right-summary .notification'
+	);
+
+	leftSideStats.forEach((leftStat, index) => {
+		const rightStat = rightSideStats[index];
+
+		const leftSideValue = parseInt(leftStat.dataset.value); 
+		const rightSideValue = parseInt(rightStat.dataset.value); 
+
+		if (rightSideValue > leftSideValue) {
+			leftStat.classList.remove('is-primary');
+			leftStat.classList.add('is-warning');
+		} else {
+			rightStat.classList.remove('is-primary');
+			rightStat.classList.add('is-warning');
+		}
+	});
+}
 
 const movieTemplate = (movieDetail) => {
+	const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+	const metaScore = parseInt(movieDetail.Metascore);
+	const imdbRating = parseFloat(movieDetail.imdbRating);
+	const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g), '');
+	let count = 0;
+	const awards = movieDetail.Awards.split(' ').reduce((prev,word) => {
+		const value = parseInt(word)
+		if(isNaN(value)){
+			return prev
+		}else{
+			return prev + value
+		}
+	},0)
+	console.log(awards)
 	return `
     <article class="media">
       <figure class="media-left">
@@ -86,23 +119,23 @@ const movieTemplate = (movieDetail) => {
         </div>
       </div>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${awards} class="notification is-primary">
       <p class="title">${movieDetail.Awards}</p>
       <p class="subtitle">Awards</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${dollars} class="notification is-primary">
       <p class="title">${movieDetail.BoxOffice}</p>
       <p class="subtitle">Box Office</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value =${metaScore} class="notification is-primary">
       <p class="title">${movieDetail.Metascore}</p>
       <p class="subtitle">Metascore</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${imdbRating} class="notification is-primary">
       <p class="title">${movieDetail.imdbRating}</p>
       <p class="subtitle">IMDB Rating</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${imdbVotes} class="notification is-primary">
       <p class="title">${movieDetail.imdbVotes}</p>
       <p class="subtitle">IMDB Votes</p>
     </article>
